a65d3c967a9115280835bf1e118978f5d21b70db
Laurent Thiebault <laurent.thiebault@decathlon.com>
2023-01-25 17:40:16 +0100

    config: change the update icons workflow

---
+++ build/icons/compose/com/decathlon/vitamin/compose/vitaminicons/__Fill.kt
@@ -0,0 +1,273 @@
+package com.decathlon.vitamin.compose.vitaminicons
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.decathlon.vitamin.compose.VitaminIcons
+import com.decathlon.vitamin.compose.vitaminicons.fill.Add
+import com.decathlon.vitamin.compose.vitaminicons.fill.AddCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.Alert
+import com.decathlon.vitamin.compose.vitaminicons.fill.Amazon
+import com.decathlon.vitamin.compose.vitaminicons.fill.Android
+import com.decathlon.vitamin.compose.vitaminicons.fill.Apple
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowDown
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowDownCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowGoBack
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowGoForward
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowLeft
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowLeftCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowLeftDown
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowLeftRight
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowLeftUp
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowRight
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowRightCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowRightDown
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowRightUp
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowUp
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowUpCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.ArrowUpDown
+import com.decathlon.vitamin.compose.vitaminicons.fill.Attachment
+import com.decathlon.vitamin.compose.vitaminicons.fill.Award
+import com.decathlon.vitamin.compose.vitaminicons.fill.BankCard
+import com.decathlon.vitamin.compose.vitaminicons.fill.BarChart
+import com.decathlon.vitamin.compose.vitaminicons.fill.Barcode
+import com.decathlon.vitamin.compose.vitaminicons.fill.Bill
+import com.decathlon.vitamin.compose.vitaminicons.fill.BodyScan
+import com.decathlon.vitamin.compose.vitaminicons.fill.Calendar
+import com.decathlon.vitamin.compose.vitaminicons.fill.Calendar2
+import com.decathlon.vitamin.compose.vitaminicons.fill.CalendarCheck
+import com.decathlon.vitamin.compose.vitaminicons.fill.CalendarDate
+import com.decathlon.vitamin.compose.vitaminicons.fill.Camera
+import com.decathlon.vitamin.compose.vitaminicons.fill.Car
+import com.decathlon.vitamin.compose.vitaminicons.fill.Check
+import com.decathlon.vitamin.compose.vitaminicons.fill.Checkbox
+import com.decathlon.vitamin.compose.vitaminicons.fill.CheckboxBlank
+import com.decathlon.vitamin.compose.vitaminicons.fill.CheckboxBlankCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.CheckboxCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.CheckboxMultiple
+import com.decathlon.vitamin.compose.vitaminicons.fill.ChevronDown
+import com.decathlon.vitamin.compose.vitaminicons.fill.ChevronLeft
+import com.decathlon.vitamin.compose.vitaminicons.fill.ChevronRight
+import com.decathlon.vitamin.compose.vitaminicons.fill.ChevronUp
+import com.decathlon.vitamin.compose.vitaminicons.fill.Chrome
+import com.decathlon.vitamin.compose.vitaminicons.fill.Close
+import com.decathlon.vitamin.compose.vitaminicons.fill.CloseCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.Cloud
+import com.decathlon.vitamin.compose.vitaminicons.fill.CloudWindy
+import com.decathlon.vitamin.compose.vitaminicons.fill.Counter
+import com.decathlon.vitamin.compose.vitaminicons.fill.CustomerService
+import com.decathlon.vitamin.compose.vitaminicons.fill.Database
+import com.decathlon.vitamin.compose.vitaminicons.fill.DeleteBin
+import com.decathlon.vitamin.compose.vitaminicons.fill.Direction
+import com.decathlon.vitamin.compose.vitaminicons.fill.Distance
+import com.decathlon.vitamin.compose.vitaminicons.fill.Download
+import com.decathlon.vitamin.compose.vitaminicons.fill.Drive
+import com.decathlon.vitamin.compose.vitaminicons.fill.Drizzle
+import com.decathlon.vitamin.compose.vitaminicons.fill.Earth
+import com.decathlon.vitamin.compose.vitaminicons.fill.Edit
+import com.decathlon.vitamin.compose.vitaminicons.fill.Emotion
+import com.decathlon.vitamin.compose.vitaminicons.fill.EmotionLaugh
+import com.decathlon.vitamin.compose.vitaminicons.fill.EmotionNormal
+import com.decathlon.vitamin.compose.vitaminicons.fill.EmotionSad
+import com.decathlon.vitamin.compose.vitaminicons.fill.EmotionUnhappy
+import com.decathlon.vitamin.compose.vitaminicons.fill.Equalizer
+import com.decathlon.vitamin.compose.vitaminicons.fill.ErrorWarning
+import com.decathlon.vitamin.compose.vitaminicons.fill.Exchange
+import com.decathlon.vitamin.compose.vitaminicons.fill.ExternalLink
+import com.decathlon.vitamin.compose.vitaminicons.fill.Eye
+import com.decathlon.vitamin.compose.vitaminicons.fill.EyeOff
+import com.decathlon.vitamin.compose.vitaminicons.fill.Facebook
+import com.decathlon.vitamin.compose.vitaminicons.fill.Feedback
+import com.decathlon.vitamin.compose.vitaminicons.fill.File
+import com.decathlon.vitamin.compose.vitaminicons.fill.FileCopy
+import com.decathlon.vitamin.compose.vitaminicons.fill.Film
+import com.decathlon.vitamin.compose.vitaminicons.fill.Filter
+import com.decathlon.vitamin.compose.vitaminicons.fill.FilterOff
+import com.decathlon.vitamin.compose.vitaminicons.fill.Fire
+import com.decathlon.vitamin.compose.vitaminicons.fill.Firefox
+import com.decathlon.vitamin.compose.vitaminicons.fill.Flashlight
+import com.decathlon.vitamin.compose.vitaminicons.fill.Focus
+import com.decathlon.vitamin.compose.vitaminicons.fill.Foggy
+import com.decathlon.vitamin.compose.vitaminicons.fill.Football
+import com.decathlon.vitamin.compose.vitaminicons.fill.Forbid
+import com.decathlon.vitamin.compose.vitaminicons.fill.Fullscreen
+import com.decathlon.vitamin.compose.vitaminicons.fill.Function
+import com.decathlon.vitamin.compose.vitaminicons.fill.Gift
+import com.decathlon.vitamin.compose.vitaminicons.fill.Github
+import com.decathlon.vitamin.compose.vitaminicons.fill.Google
+import com.decathlon.vitamin.compose.vitaminicons.fill.Grid
+import com.decathlon.vitamin.compose.vitaminicons.fill.Group
+import com.decathlon.vitamin.compose.vitaminicons.fill.Hail
+import com.decathlon.vitamin.compose.vitaminicons.fill.Heart
+import com.decathlon.vitamin.compose.vitaminicons.fill.HeartPulse
+import com.decathlon.vitamin.compose.vitaminicons.fill.History
+import com.decathlon.vitamin.compose.vitaminicons.fill.Home
+import com.decathlon.vitamin.compose.vitaminicons.fill.Hourglass
+import com.decathlon.vitamin.compose.vitaminicons.fill.Ie
+import com.decathlon.vitamin.compose.vitaminicons.fill.Image
+import com.decathlon.vitamin.compose.vitaminicons.fill.Information
+import com.decathlon.vitamin.compose.vitaminicons.fill.Instagram
+import com.decathlon.vitamin.compose.vitaminicons.fill.KakaoTalk
+import com.decathlon.vitamin.compose.vitaminicons.fill.Leaf
+import com.decathlon.vitamin.compose.vitaminicons.fill.Lightbulb
+import com.decathlon.vitamin.compose.vitaminicons.fill.Line
+import com.decathlon.vitamin.compose.vitaminicons.fill.LineChart
+import com.decathlon.vitamin.compose.vitaminicons.fill.Link
+import com.decathlon.vitamin.compose.vitaminicons.fill.ListCheck
+import com.decathlon.vitamin.compose.vitaminicons.fill.ListSettings
+import com.decathlon.vitamin.compose.vitaminicons.fill.Lock
+import com.decathlon.vitamin.compose.vitaminicons.fill.Logout
+import com.decathlon.vitamin.compose.vitaminicons.fill.Mac
+import com.decathlon.vitamin.compose.vitaminicons.fill.Macbook
+import com.decathlon.vitamin.compose.vitaminicons.fill.Mail
+import com.decathlon.vitamin.compose.vitaminicons.fill.MailSend
+import com.decathlon.vitamin.compose.vitaminicons.fill.MapPin
+import com.decathlon.vitamin.compose.vitaminicons.fill.Mastercard
+import com.decathlon.vitamin.compose.vitaminicons.fill.Menu
+import com.decathlon.vitamin.compose.vitaminicons.fill.Message
+import com.decathlon.vitamin.compose.vitaminicons.fill.Messenger
+import com.decathlon.vitamin.compose.vitaminicons.fill.Mic
+import com.decathlon.vitamin.compose.vitaminicons.fill.Mist
+import com.decathlon.vitamin.compose.vitaminicons.fill.MoneyCnyBox
+import com.decathlon.vitamin.compose.vitaminicons.fill.MoneyDollarBox
+import com.decathlon.vitamin.compose.vitaminicons.fill.MoneyEuroBox
+import com.decathlon.vitamin.compose.vitaminicons.fill.MoneyPoundBox
+import com.decathlon.vitamin.compose.vitaminicons.fill.Moon
+import com.decathlon.vitamin.compose.vitaminicons.fill.MoonCloudy
+import com.decathlon.vitamin.compose.vitaminicons.fill.More
+import com.decathlon.vitamin.compose.vitaminicons.fill.More2
+import com.decathlon.vitamin.compose.vitaminicons.fill.Music
+import com.decathlon.vitamin.compose.vitaminicons.fill.Music2
+import com.decathlon.vitamin.compose.vitaminicons.fill.Naver
+import com.decathlon.vitamin.compose.vitaminicons.fill.Navigation
+import com.decathlon.vitamin.compose.vitaminicons.fill.Notification
+import com.decathlon.vitamin.compose.vitaminicons.fill.Package
+import com.decathlon.vitamin.compose.vitaminicons.fill.Pause
+import com.decathlon.vitamin.compose.vitaminicons.fill.PauseCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.Phone
+import com.decathlon.vitamin.compose.vitaminicons.fill.PieChart
+import com.decathlon.vitamin.compose.vitaminicons.fill.Pinterest
+import com.decathlon.vitamin.compose.vitaminicons.fill.Play
+import com.decathlon.vitamin.compose.vitaminicons.fill.PlayCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.PriceTag
+import com.decathlon.vitamin.compose.vitaminicons.fill.Printer
+import com.decathlon.vitamin.compose.vitaminicons.fill.QrScan
+import com.decathlon.vitamin.compose.vitaminicons.fill.Question
+import com.decathlon.vitamin.compose.vitaminicons.fill.RadioButton
+import com.decathlon.vitamin.compose.vitaminicons.fill.Rainy
+import com.decathlon.vitamin.compose.vitaminicons.fill.Recycle
+import com.decathlon.vitamin.compose.vitaminicons.fill.Refresh
+import com.decathlon.vitamin.compose.vitaminicons.fill.Refund
+import com.decathlon.vitamin.compose.vitaminicons.fill.Repeat
+import com.decathlon.vitamin.compose.vitaminicons.fill.Return
+import com.decathlon.vitamin.compose.vitaminicons.fill.Riding
+import com.decathlon.vitamin.compose.vitaminicons.fill.Rocket
+import com.decathlon.vitamin.compose.vitaminicons.fill.Rss
+import com.decathlon.vitamin.compose.vitaminicons.fill.Ruler
+import com.decathlon.vitamin.compose.vitaminicons.fill.Run
+import com.decathlon.vitamin.compose.vitaminicons.fill.Safari
+import com.decathlon.vitamin.compose.vitaminicons.fill.Scales
+import com.decathlon.vitamin.compose.vitaminicons.fill.Search
+import com.decathlon.vitamin.compose.vitaminicons.fill.SecurePayment
+import com.decathlon.vitamin.compose.vitaminicons.fill.Seed1
+import com.decathlon.vitamin.compose.vitaminicons.fill.Seed2
+import com.decathlon.vitamin.compose.vitaminicons.fill.Seed3
+import com.decathlon.vitamin.compose.vitaminicons.fill.SelectedByDecathlon
+import com.decathlon.vitamin.compose.vitaminicons.fill.SendPlane
+import com.decathlon.vitamin.compose.vitaminicons.fill.SendPlane2
+import com.decathlon.vitamin.compose.vitaminicons.fill.Settings
+import com.decathlon.vitamin.compose.vitaminicons.fill.Share
+import com.decathlon.vitamin.compose.vitaminicons.fill.ShareAndroid
+import com.decathlon.vitamin.compose.vitaminicons.fill.ShareIos
+import com.decathlon.vitamin.compose.vitaminicons.fill.ShieldCheck
+import com.decathlon.vitamin.compose.vitaminicons.fill.ShoppingBag
+import com.decathlon.vitamin.compose.vitaminicons.fill.ShoppingCart
+import com.decathlon.vitamin.compose.vitaminicons.fill.Showers
+import com.decathlon.vitamin.compose.vitaminicons.fill.ShutDown
+import com.decathlon.vitamin.compose.vitaminicons.fill.Slack
+import com.decathlon.vitamin.compose.vitaminicons.fill.Smartphone
+import com.decathlon.vitamin.compose.vitaminicons.fill.Snapchat
+import com.decathlon.vitamin.compose.vitaminicons.fill.Snowy
+import com.decathlon.vitamin.compose.vitaminicons.fill.Sparkles
+import com.decathlon.vitamin.compose.vitaminicons.fill.Star
+import com.decathlon.vitamin.compose.vitaminicons.fill.StarHalf
+import com.decathlon.vitamin.compose.vitaminicons.fill.Stop
+import com.decathlon.vitamin.compose.vitaminicons.fill.StopCircle
+import com.decathlon.vitamin.compose.vitaminicons.fill.Store
+import com.decathlon.vitamin.compose.vitaminicons.fill.Subtract
+import com.decathlon.vitamin.compose.vitaminicons.fill.Suitcase
+import com.decathlon.vitamin.compose.vitaminicons.fill.Sun
+import com.decathlon.vitamin.compose.vitaminicons.fill.SunCloudy
+import com.decathlon.vitamin.compose.vitaminicons.fill.TShirt
+import com.decathlon.vitamin.compose.vitaminicons.fill.Tablet
+import com.decathlon.vitamin.compose.vitaminicons.fill.Task
+import com.decathlon.vitamin.compose.vitaminicons.fill.Telegram
+import com.decathlon.vitamin.compose.vitaminicons.fill.TempCold
+import com.decathlon.vitamin.compose.vitaminicons.fill.TempHot
+import com.decathlon.vitamin.compose.vitaminicons.fill.ThumbDown
+import com.decathlon.vitamin.compose.vitaminicons.fill.ThumbUp
+import com.decathlon.vitamin.compose.vitaminicons.fill.Thunderstorms
+import com.decathlon.vitamin.compose.vitaminicons.fill.Tiktok
+import com.decathlon.vitamin.compose.vitaminicons.fill.Time
+import com.decathlon.vitamin.compose.vitaminicons.fill.Timer
+import com.decathlon.vitamin.compose.vitaminicons.fill.Tools
+import com.decathlon.vitamin.compose.vitaminicons.fill.Train
+import com.decathlon.vitamin.compose.vitaminicons.fill.Trophy
+import com.decathlon.vitamin.compose.vitaminicons.fill.Truck
+import com.decathlon.vitamin.compose.vitaminicons.fill.Twitter
+import com.decathlon.vitamin.compose.vitaminicons.fill.Unlink
+import com.decathlon.vitamin.compose.vitaminicons.fill.Upload
+import com.decathlon.vitamin.compose.vitaminicons.fill.User
+import com.decathlon.vitamin.compose.vitaminicons.fill.Viber
+import com.decathlon.vitamin.compose.vitaminicons.fill.Visa
+import com.decathlon.vitamin.compose.vitaminicons.fill.VolumeAlert
+import com.decathlon.vitamin.compose.vitaminicons.fill.VolumeDown
+import com.decathlon.vitamin.compose.vitaminicons.fill.VolumeMute
+import com.decathlon.vitamin.compose.vitaminicons.fill.VolumeUp
+import com.decathlon.vitamin.compose.vitaminicons.fill.Walk
+import com.decathlon.vitamin.compose.vitaminicons.fill.Wallet
+import com.decathlon.vitamin.compose.vitaminicons.fill.Whatsapp
+import com.decathlon.vitamin.compose.vitaminicons.fill.Windy
+import com.decathlon.vitamin.compose.vitaminicons.fill.Youtube
+import com.decathlon.vitamin.compose.vitaminicons.fill.ZoomIn
+import com.decathlon.vitamin.compose.vitaminicons.fill.ZoomOut
+import kotlin.collections.List as ____KtList
+
+public object FillGroup
+
+public val VitaminIcons.Fill: FillGroup
+  get() = FillGroup
+
+private var __AllAssets: ____KtList<ImageVector>? = null
+
+public val FillGroup.AllAssets: ____KtList<ImageVector>
+  get() {
+    if (__AllAssets != null) {
+      return __AllAssets!!
+    }
+    __AllAssets= listOf(ChevronUp, Line, Grid, Film, ArrowRight, Rainy, DeleteBin, Foggy, Instagram,
+        CheckboxBlank, Notification, ThumbUp, ShareAndroid, Recycle, Chrome, Feedback, Upload,
+        Android, SendPlane, Amazon, ArrowGoForward, LineChart, Refund, Edit, CalendarDate, Safari,
+        Snapchat, FilterOff, SendPlane2, Unlink, ArrowLeftDown, User, ArrowRightUp, ArrowLeftRight,
+        Leaf, Question, Subtract, Windy, ShareIos, TempCold, Play, Showers, Counter, Drive, Truck,
+        ShoppingBag, RadioButton, Messenger, History, ZoomOut, Rss, Lock, Share, Award, Add,
+        Telegram, Hail, MoonCloudy, FileCopy, Mac, Google, ShoppingCart, StopCircle, ListSettings,
+        VolumeDown, Fire, PieChart, Mastercard, ExternalLink, Calendar, CheckboxMultiple,
+        MoneyCnyBox, CheckboxCircle, Car, ShutDown, PlayCircle, MapPin, Moon, Eye, EmotionNormal,
+        StarHalf, Thunderstorms, Wallet, Logout, Bill, ArrowLeftUp, Navigation, ArrowUp, Mail,
+        Earth, Phone, VolumeAlert, Database, Emotion, HeartPulse, Mist, ChevronRight, Repeat,
+        Download, Home, Macbook, Facebook, Stop, Group, Return, Forbid, Ie, EmotionSad, ChevronLeft,
+        Naver, Run, Cloud, Seed3, Hourglass, Refresh, ChevronDown, ZoomIn, Scales, EmotionLaugh,
+        Sparkles, Flashlight, Task, Package, Tiktok, KakaoTalk, Seed1, Checkbox, More2, Link, Gift,
+        SunCloudy, VolumeUp, MoneyPoundBox, Music, Store, Attachment, Pinterest, Close, BankCard,
+        Direction, TempHot, Whatsapp, Riding, Information, ShieldCheck, SecurePayment, Ruler,
+        CloseCircle, Lightbulb, EyeOff, Menu, ArrowDownCircle, Alert, ArrowLeft, Equalizer,
+        SelectedByDecathlon, Snowy, MailSend, Suitcase, Exchange, Calendar2, PauseCircle,
+        MoneyDollarBox, ArrowGoBack, Github, Smartphone, ArrowUpDown, MoneyEuroBox, Barcode, Viber,
+        Mic, BodyScan, Pause, Distance, Time, ArrowDown, Tools, CheckboxBlankCircle, Message,
+        ArrowRightCircle, BarChart, Seed2, Timer, Visa, ArrowUpCircle, CustomerService, Football,
+        CalendarCheck, VolumeMute, Image, Drizzle, Check, ErrorWarning, Fullscreen, Camera,
+        PriceTag, Slack, Focus, Settings, Function, Youtube, ArrowLeftCircle, More, Tablet, Sun,
+        Heart, ThumbDown, File, Printer, Walk, Rocket, ListCheck, Firefox, Star, Search, TShirt,
+        CloudWindy, Apple, Train, Music2, ArrowRightDown, Filter, Twitter, EmotionUnhappy,
+        AddCircle, Trophy, QrScan)
+    return __AllAssets!!
+  }
